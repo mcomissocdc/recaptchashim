3803150cdf2c3b7ce7fed65e9679f8c9a9a88148
dchatzieleftheriou-bc <dchatzieleftheriou@blockchain.com>
2021-07-01 19:23:02 +0300

    updates

--- recaptcha/RecapatchaToken.m
+++
@@ -1,8 +0,0 @@
-//
-//  RecapatchaToken.m
-//  recaptchashim
-//
-//  Created by Dimitrios Chatzieleftheriou on 30/06/2021.
-//
-
-#import <Foundation/Foundation.h>
--- recaptcha/recaptcha.h
+++ recaptcha/recaptcha.h
@@ -1,13 +1,3 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for recaptchashim.
-FOUNDATION_EXPORT double recaptchashimVersionNumber;
-
-//! Project version string for recaptchashim.
-FOUNDATION_EXPORT const unsigned char recaptchashimVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <recaptchashim/PublicHeader.h>
-
 #import "RCARecaptchaError.h"
 #import "RecaptchaAction.h"
 #import "RecaptchaActionType.h"
